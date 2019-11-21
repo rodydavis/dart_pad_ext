87e5fd19cfc3243ff32bd899040550a3f40b1814
Rody Davis <rody.davis.jr@gmail.com>
2019-11-21 10:39:15 -0500

    refactor

---
+++ templates/tool/script.js
@@ -0,0 +1,7 @@
+const anchor = document.createElement('a');
+anchor.href = 'https://dartpad.dev/embed-flutter.html?id=468b77f8f1e6de58f6e3eee9582502ef';
+anchor.innerText = 'Flutter';
+anchor.style.marginLeft = '10px';
+
+var dartButton = document.getElementsByTagName("footer")[0];
+dartButton.appendChild(anchor);
